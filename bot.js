2b3b244c02bce1ecca0f7d07e011155a1985460a
Noah <37852935+Syncoman@users.noreply.github.com>
2018-04-16 23:29:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Client({ disableEveryone: true});
 const fs = require('fs');
 client.config = require('./config.json');
 client.login(client.config.token);
-// client.db = require('rethinkdbdash')({ db: 'sync' });
 client.error = require('./util/errorLogger.js').run;
 client.tempProfiles = {};
 exports.client = client;
@@ -11,16 +10,18 @@ exports.client = client;
 client.on("error", (O_o) => {});
 
 client.on("guildMemberAdd", (user) => {
-    let fireTime = new Date();
+    let infoChannel = client.channels.get("420407754327457792");
+    let suggestChannel = client.channels.get("420411538449498113");
+    let botChannel = client.channels.get("399916099325657088");
     let channel = user.guild.channels.find('name', 'welcome');
     if (!channel) return;
-    channel.send(`Welcome ${user.user} to the United Federations of Food!\nCurrent Member Count: ${user.guild.memberCount}`);
+    channel.send(`Everyone welcome ${user.user} to United Federations of Food!\nCurrent Member Count: ${user.guild.memberCount}`);
+    user.send(`**Hello!**\n\nThank you for joining United Federations of Food. Please take a moment to familiarize yourself with our rules located in ${infoChannel}\nIf you play a certain game you can do ?rank (game here) in ${botChannel} to get that role so you can become apart of that games branch\nIf you play a game and we don't have a rank for it you can make a suggestion in ${suggestChannel}\nFeel free to contact one of our staff members if you need help with anything. A list of staff members is located in ${infoChannel}\n\n*We hope you have a good time!*\n*-UFF Staff Team*`)
     var role = user.guild.roles.find('name', 'Members');
     user.addRole(role)
 });
 
 client.on("guildMemberRemove", (user) => {
-    let fireTime = new Date();
     let channel = user.guild.channels.find('name', 'leave');
     if (!channel) return;
     channel.send(`**${user.user.tag}** has left!\nCurrent Member Count: ${user.guild.memberCount}`);
@@ -34,3 +35,4 @@ fs.readdir('./events', (err, files) => {
         client.on(eventName, (...args) => eventFunction.run(client, ...args));
     });
 });
+
