11e5cb57a8cf7a634293ec250b16078697ebc3e6
Noah <37852935+Syncoman@users.noreply.github.com>
2018-04-22 00:55:53 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,18 +2,29 @@ const Discord = require("discord.js");
 
 module.exports = {
     run: async (client, msg, args) => {
-        const embed = new Discord.RichEmbed()
-        .setAuthor("Food BOT Commands", client.user.avatarURL)
-        .addField("System", "+help - List help commands\n+ping - Checks the bots ping")
-        .addField("Regular", "+food - Daily food command\n+info - Information about the United Federations of Food")
-        .addField("Staff", "+kick - Kick a user from the server\n+ban - Ban a user from the server\n+purge - Purge a certain amount of messages")
+        if (!args[0]) {
+        const help = new Discord.RichEmbed()
         .setColor(0x42f471)
-        await msg.channel.send( {embed} );
-},
+        .setAuthor("FoodBOT Commands", client.user.avatarURL)
+        .addField("Regular", "+ping - Shows the bots ping\n+help - Commands for the bot\n+food - Random food generation\n+info - Information about the United Federations of Food")
+        .setFooter(`If you are a staff member do +help staff`)
+            msg.channel.send(help);
+        } else if (args[0].toLowerCase() === 'staff') {
+        const staff = new Discord.RichEmbed()
+        .setColor(0x42f471)
+        .setAuthor("FoodBOT Staff Commands", client.user.avatarURL)
+        .addField("Staff", "+kick - Kick a user from the server\n+ban - Ban a user from the server\n+warn - Warns a user\n+purge - Purge a certain amount of messages")
+        .setFooter(`Do +help for normal commands`)
+            msg.channel.send(staff);
+        } else {
+            msg.channel.send(`Error`);
+        }
+        
+    },
     meta: {
         name: 'help',
         ownerOnly: false,
-        description: 'List of commands',
+        description: 'Need help?',
         usage: ''
     }
 }
