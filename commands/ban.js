7d232b06b8345b0e8485976e150b99c23e6f7806
Noah <syncomanx@gmail.com>
2018-05-14 19:33:12 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,25 +1,28 @@
+const Discord = require("discord.js");
+
 module.exports = {
     run: async (client, msg, args) => {
-        let user = msg.mentions.members.first();
-        if (!msg.guild.me.hasPermission("BAN_MEMBERS")) return await msg.channel.send("SyncBOT does not have \`BAN_MEMBERS\` permission!");
-        if (!msg.member.hasPermission("BAN_MEMBERS")) return await msg.channel.send("You do not have \`BAN_MEMBERS\` permission!");
-        if (!user) user = msg.guild.members.get(args[0]);
-        if (!user) return await msg.channel.send("Please mention a valid user to ban");
-        if (!user.bannable) return await msg.channel.send("User cannot be banned"); 
-        const reason = args.slice(1).join(" ");
-        if (reason) {
-        await user.ban(`Banned by ${msg.author.tag} - ${reason}`);
-        await msg.channel.send(`**${user.user.tag}** has been banned for '${reason}'`);
-        let channel = user.guild.channels.find('name', 'moderation_logs');
-        if (!channel) return;
-        channel.send(`**${user.user.tag}** banned by **${msg.author.tag}** - **Reason:** ${reason}`);
-        } else if (!reason) {
-        await user.ban(`Banned by ${msg.author.tag} - No reason specified`);
-        await msg.channel.send(`**${user.user.tag}** has been banned with no specified reason`); 
-        let channel = user.guild.channels.find('name', 'moderation_logs');
-        if (!channel) return;
-        channel.send(`**${user.user.tag}** banned by **${msg.author.tag}** - **Reason:** None Provided`);
-      }
+      let user = msg.mentions.members.first();
+      if (!msg.guild.me.hasPermission("BAN_MEMBERS")) return await msg.channel.send("Bot does not have \`BAN_MEMBERS\` permission!");
+      if (!msg.member.hasPermission("BAN_MEMBERS")) return await msg.channel.send("You do not have \`BAN_MEMBERS\` permission!");
+      if (!user) user = msg.guild.members.get(args[0]);
+      if (!user) return await msg.channel.send("Please mention a valid user to ban.");
+      if (!user.kickable) return await msg.channel.send("User cannot be banned.");
+      const reason = args.slice(1).join(" ");
+      if (!reason) return msg.channel.send("Please provide a reason for the ban.");
+      let channel = user.guild.channels.find('name', 'mod_logs');
+      if (!channel) return;
+      await msg.channel.send(`:white_check_mark: ***${user.user.tag}** has been banned.*`);
+      await user.ban(`${user.user.tag} has been banned for '${reason}'`);
+      const embed = new Discord.RichEmbed()
+      .setColor(0x42f471)
+      .setAuthor(`Ban | ${user.user.tag}`, client.user.avatarURL)
+      .addField("User", `${user.user.tag}`, true)
+      .addField("Moderator", `${msg.author.tag}`, true)
+      .addField("Reason", `${reason}`, true)
+      .setFooter('UFF Moderation')
+      .setTimestamp()
+      await channel.send( {embed} );
     },
     meta: {
         name: 'ban',
