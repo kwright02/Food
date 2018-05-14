ccd6c5f62a8199bd849716aeb115242c593b48e8
Noah <syncomanx@gmail.com>
2018-05-14 19:33:43 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,25 +1,28 @@
+const Discord = require("discord.js");
+
 module.exports = {
     run: async (client, msg, args) => {
         let user = msg.mentions.members.first();
         if (!msg.guild.me.hasPermission("KICK_MEMBERS")) return await msg.channel.send("Bot does not have \`KICK_MEMBERS\` permission!");
         if (!msg.member.hasPermission("KICK_MEMBERS")) return await msg.channel.send("You do not have \`KICK_MEMBERS\` permission!");
         if (!user) user = msg.guild.members.get(args[0]);
-        if (!user) return await msg.channel.send("Please mention a valid user to kick");
-        if (!user.kickable) return await msg.channel.send("User cannot be kicked"); 
+        if (!user) return await msg.channel.send("Please mention a valid user to kick.");
+        if (!user.kickable) return await msg.channel.send("User cannot be kicked.");
         const reason = args.slice(1).join(" ");
-        if (reason) {
-        await user.kick(`Kicked by ${msg.author.tag} - ${reason}`);
-        await msg.channel.send(`**${user.user.tag}** has been kicked for '${reason}'`);
-        let channel = user.guild.channels.find('name', 'moderation_logs');
+        if (!reason) return msg.channel.send("Please provide a reason for the kick.");
+        let channel = user.guild.channels.find('name', 'mod_logs');
         if (!channel) return;
-        channel.send(`**${user.user.tag}** kicked by **${msg.author.tag}** - **Reason:** ${reason}`);
-        } else if (!reason) {
-        await user.kick(`Kicked by ${msg.author.tag} - No reason specified`);
-        await msg.channel.send(`**${user.user.tag}** has been kicked with no specified reason`); 
-        let channel = msg.guild.channels.find('name', 'moderation_logs');
-        if (!channel) return;
-        channel.send(`**${user.user.tag}** kicked by **${msg.author.tag}** - **Reason:** None Provided`);
-      }
+        await msg.channel.send(`:white_check_mark: ***${user.user.tag}** has been kicked.*`);
+        await user.kick(`${user.user.tag} has been kicked for '${reason}'`);
+        const embed = new Discord.RichEmbed()
+        .setColor(0x42f471)
+        .setAuthor(`Kick | ${user.user.tag}`, client.user.avatarURL)
+        .addField("User", `${user.user.tag}`, true)
+        .addField("Moderator", `${msg.author.tag}`, true)
+        .addField("Reason", `${reason}`, true)
+        .setFooter('UFF Moderation')
+        .setTimestamp()
+        await channel.send( {embed} );
     },
     meta: {
         name: 'kick',
