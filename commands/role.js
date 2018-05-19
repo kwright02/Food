3f0efe78a89f5c77edfb434c83cbe7d591c6c4fd
Noah <syncomanx@gmail.com>
2018-05-18 22:31:26 -0400

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,6 +1,6 @@
 module.exports = {
     run: async (client, msg, args) => {
-        const roles = ["Ark", "Battlefield", "Castle Crasher", "Call of Duty", "Crafting Dead (Minecraft)", "CS:GO", "Destiny", "DOOM", "Fallout", "Fortnite", "Garry's Mod", "Grand Theft Auto", "Golf It", "Hand Simulator", "Hearhtstone", "H1Z1", "League of Legends", "Minecraft", "Modded Minecraft", "Overwatch", "Payday", "Politics and War", "Rocket League", "PUBG", "Rainbow Six Seige", "Roblox", "Rust", "Skyrim", "Team Fortress 2", "Town of Salem", "Terraria", "Unturned", "VR Chat", "War thunder", "Watchdogs", "World of Tanks", "Spotify"];
+        const roles = ["Ark", "Battlefield", "Castle Crasher", "Call of Duty", "Crafting Dead (Minecraft)", "CS:GO", "Destiny", "DOOM", "Fallout", "Fortnite", "Garry's Mod", "Golf It", "Hand Simulator", "Hearhtstone", "H1Z1", "League of Legends", "Minecraft", "Payday", "Politics and War", "PUBG", "Rainbow Six Seige", "Rust", "Skyrim", "Team Fortress 2", "Terraria", "VR Chat", "War thunder", "Watchdogs", "World of Tanks"];
         let role = msg.guild.roles.find('name', args.join(" "));
         if (!roles.includes(args.join(" "))) return msg.channel.send("Either role doesn't exist or you're not using capital letters in the correct spots!");
         if (!args[0]) return msg.channel.send("Please enter a role to join!");
