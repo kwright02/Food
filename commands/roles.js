6ab83c93d0383983b9dd48623e86423616030200
Noah <syncomanx@gmail.com>
2018-05-18 22:30:58 -0400

    Update roles.js

--- commands/roles.js
+++ commands/roles.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
     run: async (client, msg, args) => {
-        const roles = ["Ark", "Battlefield", "Castle Crasher", "Call of Duty", "Crafting Dead (Minecraft)", "CS:GO", "Destiny", "DOOM", "Fallout", "Fortnite", "Garry's Mod", "Grand Theft Auto", "Golf It", "Hand Simulator", "Hearhtstone", "H1Z1", "League of Legends", "Minecraft", "Modded Minecraft", "Overwatch", "Payday", "Politics and War", "Rocket League", "PUBG", "Rainbow Six Seige", "Roblox", "Rust", "Skyrim", "Team Fortress 2", "Town of Salem", "Terraria", "Unturned", "VR Chat", "War thunder", "Watchdogs", "World of Tanks", "Spotify"];
+        const roles = ["Ark", "Battlefield", "Castle Crasher", "Call of Duty", "Crafting Dead (Minecraft)", "CS:GO", "Destiny", "DOOM", "Fallout", "Fortnite", "Garry's Mod", "Golf It", "Hand Simulator", "Hearhtstone", "H1Z1", "League of Legends", "Minecraft", "Payday", "Politics and War", "PUBG", "Rainbow Six Seige", "Rust", "Skyrim", "Team Fortress 2", "Terraria", "VR Chat", "War thunder", "Watchdogs", "World of Tanks"];
         const embed = new Discord.RichEmbed()
         .setColor(0x42f471)
         .setAuthor(msg.author.username, msg.author.avatarURL)
