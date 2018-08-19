6438264eea98ce16821fb686cc5e03b4a79bf951
Noah <syncomanx@gmail.com>
2018-08-19 14:33:02 -0400

    Update command list

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = {
         .setColor(0x42f471)
         .setAuthor("FoodBOT Commands", client.user.avatarURL)
         .addField("System", "+ping - Shows the bots ping\n+help - List help commands")
-        .addField("Regular", "+food - Random food generation\n+info - Information about the United Federations of Food\n+suggest - Suggest something for the server\n+user - Information about a certain user\n+avatar - Get a users avatar")
+        .addField("Regular", "+info - Information about the United Federations of Food\n+suggest - Suggest something for the server\n+user - Information about a certain user\n+avatar - Get a users avatar")
         .addField("Fun", "+wyr - Would you rather, this or that\n+joke - Tells a joke\n+ship - Ship 2 users together\n+emojify - Turn your message into emojis")
         .addField("Apply", "+applymod - Apply for moderator\n+applybm - Apply for branch manager")
         .addField("Staff", "+warn - Warn a user\n+kick - Kick a user\n+ban - Ban a user\n+punishments - List a users punishments\n+perms - Manage user permissions\n+purge - Purge a certain amount of messages")
