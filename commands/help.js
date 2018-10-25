b83a0cffb260307e68460e8c302b8d0108968e44
Noah <syncomanx@gmail.com>
2018-10-25 17:55:52 -0400

    Updated help command

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
         .addField("System", "+ping - Shows the bots ping\n+help - List help commands")
         .addField("Regular", "+info - Information about the United Federations of Food\n+suggest - Suggest something for the server\n+user - Information about a certain user\n+avatar - Get a users avatar\n+applymod - Apply for moderator\n+applybm - Apply for branch manager")
         .addField("Fun", "+stats - Check a users xp, level, and food points\n+food - Claim your daily Food Points\n+wyr - Would you rather, this or that\n+joke - Tells a joke\n+ship - Ship 2 users together\n+emojify - Turn your message into emojis")
-        .addField("Staff", "+warn - Warn a user\n+kick - Kick a user\n+ban - Ban a user\n+punishments - List a users punishments\n+perms - Manage a users permissions\n+purge - Purge a certain amount of messages\n+pts - Manage a users Food Points")
+        .addField("Staff", "+warn - Warn a user\n+kick - Kick a user\n+ban - Ban a user\n+perms - Manage a users permissions\n+purge - Purge a certain amount of messages\n+pts - Manage a users Food Points")
             msg.channel.send(help);
     },
     meta: {
