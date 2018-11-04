27b48f5115db81e86ee632118b3c032f507cf6f3
Noah <syncomanx@gmail.com>
2018-11-04 13:48:31 -0500

    Flip command

---
+++ commands/flip.js
@@ -0,0 +1,12 @@
+module.exports = {
+    run: async (client, msg, args) => {
+let choices = ["Heads", "Tails"];
+let random = choices[Math.floor(Math.random()*choices.length)];
+      msg.channel.send(random)
+    },
+    meta: {
+        name: 'coinflip',
+        description: 'Flip a coin',
+        usage: ''
+    }
+}
