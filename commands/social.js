fd7a8cf6e78d92f3bdf384b1fb72fe93fc26117a
Kyle Wright <kylewright@JDXs-MBP.lan>
2018-12-16 09:56:09 -0600

    Added sponsors command

---
+++ commands/social.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    run: async (client, msg, args) => {
+      var channel = msg.channel;
+      channel.send(`––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+:information_source: UFF Social Information :information_source:
+
+Twitter: https://twitter.com/FederationsOf
+Steam Community: https://steamcommunity.com/groups/UnitedFederationsofFood#
+Kinguin: https://www.kinguin.net/?r=54545 Discount Code: UFF
+Stimpacks: https://stimpacks.com/?utm_source=affiliate&utm_campaign=UFF. Discount Code: UFF
+
+If you’re interested in sponsoring UFF, please contact an Admin or Higher
+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––`)
+},
+meta: {
+    name: 'ship',
+    description: 'Ship 2 users',
+    usage: ''
+    }
+ }
